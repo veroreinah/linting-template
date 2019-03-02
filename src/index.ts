fceac08f7cbcbd44e200648cfee4263394ef73e2
Verónica Reina <veroreinah@gmail.com>
2019-03-02 17:58:18 +0100

    Example files

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+const test = 'blabla';
+
+const test2 = 'blabla double quotes';
+var blabla = 'blabla';
+
+function init({
+  option1,
+  option2,
+  option3,
+  option4,
+  option5,
+  option6,
+  option7,
+}) {
+  //...
+}
+
+function sayHi(name) {
+  return 'Result: ' + name;
+}
